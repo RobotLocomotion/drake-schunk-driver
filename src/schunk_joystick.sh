e68fc44cf2b11009494f64c58151034cdc36aa95
Grant Gould <ggould@tri.global>
2016-09-15 16:04:03 -0400

    More clearly document mappings

--- src/schunk_joystick.sh
+++ src/schunk_joystick.sh
@@ -20,8 +20,12 @@ set -ex
 export LD_LIBRARY_PATH=$LCM_SO_DIR:$LD_LIBRARY_PATH
 export PYTHONPATH=$LCM_PY_DIR:$PYTHONPATH
 
+# Map the left stick x axis to position from 1 to 99 (mm).
+POSITION_MAPPING="--mapping 0 target_position_mm 50 49"
+
+# Map the left stick y axis to force from 1 to 79 (N).
+FORCE_MAPPING="--mapping 1 force 40 -39"
+
 $LCM_JOYSTICK_CONTROL_CMD \
     $SCHUNK_LCMT_DIR lcmt_schunk_command --lcm_tag SCHUNK_COMMAND \
-    --mapping 0 target_position_mm 50 49 \
-    --mapping 1 force 40 -39 \
-    --quit-on 9
+    $POSITION_MAPPING $FORCE_MAPPING --quit-on 9
