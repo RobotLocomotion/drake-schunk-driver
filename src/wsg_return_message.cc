96ac52008ce456964cc992e888ff6408fd80917a
Jeremy Nimmer <jeremy.nimmer@tri.global>
2018-04-02 16:53:09 -0400

    Resolve some GCC warnings
    -Wreturn-type
    -Wsign-compare
    -Wunused-variable

--- src/wsg_return_message.cc
+++ src/wsg_return_message.cc
@@ -14,9 +14,10 @@ WsgReturnMessage::Parse(std::vector<unsigned char>& buffer) {
   int command = buffer[3];
   int payload_size = buffer[4] + (buffer[5] << 8);
   assert(payload_size >= 2);
-  assert(buffer.size() == payload_size + 8);
+  assert(static_cast<int>(buffer.size()) == payload_size + 8);
   int status_code = buffer[6] + (buffer[7] << 8);
   int params_size = payload_size - 2;
+  (void)(params_size);  // (Unused.)
   std::vector<unsigned char> params(&buffer[8], &buffer[payload_size + 6]);
   // TODO(ggould-tri) Validate checksum.
   return std::unique_ptr<WsgReturnMessage>(
